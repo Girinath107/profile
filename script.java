0e2c3c2f82036f2d70a9e0da63e12569e9ac74f4
Girinath <101491768+Girinath107@users.noreply.github.com>
2022-08-01 07:09:47 +0530

    Add files via upload

---
+++ script.java
@@ -0,0 +1,17 @@
+  <script>
+        <!--hide 
+        var password
+        xyz='5'
+        pass1 = 'chinnu'
+        do{
+            password=prompt('Enter your password: ');
+            if(password==pass1){
+                xyz='1'
+            }
+            else{
+                alert('Invalid password. Please enter the correct password....');
+            }
+        }while(xyz=='5')
+        
+        -->
+    </script>
